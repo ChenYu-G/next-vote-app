b359a4d338d81099f55395fc04f165e6881ca8af
ChenYu <cgeng.charlie@gmail.com>
2024-01-03 00:27:05 -0600

    update api address

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		API_ADDRESS: 'https://next-vote-app.vercel.app/api',
+		API_ADDRESS: 'https://next-vote-app.vercel.app/',
 	},
 }
 
